abc84e70aee29c48ce2df124c971a06b9a60b748
윤효진 <hyojinee@dgsw.hs.kr>
2025-02-27 07:03:51 +0900

    feat: tanstack/react-query 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import  { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Router from './components/router';
 
+const queryClient = new QueryClient();
+
 const App = () => {
-  return (
-    <div>
-      <Router />
-    </div>
-  )
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Router />
+        </QueryClientProvider>
+    )
 }
 
 export default App;
