50042df1f98f6da9e18b77a325513005905f9785
윤효진 <hyojinee@dgsw.hs.kr>
2025-03-04 17:00:54 +0900

    feat: customAxios request interceptor

--- src/libs/axios/customAxios.ts
+++ src/libs/axios/customAxios.ts
@@ -31,3 +31,21 @@ const addRefreshSubscriber = (callback: (token: string) => void) => {
     refreshSubscribers.push(callback);
 };
 
+medinetAxios.interceptors.request.use(
+    async (config) => {
+      const token = localStorage.getItem(ACCESS_TOKEN_KEY);
+      if (token) {
+        config.headers[REQUEST_TOKEN_KEY] = `Bearer ${token}`;
+      }
+  
+      if (config.data instanceof FormData) {
+        config.headers["Content-Type"] = "multipart/form-data";
+      } else {
+        config.headers["Content-Type"] = "application/json";
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+);
