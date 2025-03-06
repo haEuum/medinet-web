9acebaca0ca34f95b95e3995ad4fc890699866f5
윤효진 <hyojinee@dgsw.hs.kr>
2025-03-07 08:26:31 +0900

    commit

--- src/libs/axios/customAxios.ts
+++ src/libs/axios/customAxios.ts
@@ -2,6 +2,34 @@ import axios, { AxiosRequestConfig } from 'axios';
 import { requestInterceptor } from './requestInterceptor';
 import  { responseErrorInterceptor } from './responseErrorInterceptor';
 import { ErrorHandler } from './errorHandler';
-import { ACCESS_TOKEN, REFRESH_TOKEN } from '@/constants/token/token';
+import { ACCESS_TOKEN, REQUEST_TOKEN } from '@/constants/token/token';
 import Token from '@/libs/token/token';
-import { SERVER_URL } from '@env';
+
+const createCustomAxiosInstance = (baseURL?: AxiosRequestConfig) => {
+    return axios.create({
+        withCredentials: true,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+        },
+    });
+};
+
+export const medinetAxios = createCustomAxiosInstance({
+    baseURL: process.env.SERVER_URL,
+    headers: {
+        [REQUEST_TOKEN]: `Bearer ${Token.getToken(ACCESS_TOKEN)}`,
+    },
+});
+
+medinetAxios.interceptors.request.use(requestInterceptor);
+medinetAxios.interceptors.response.use(
+    (response) => response,
+    responseErrorInterceptor
+);
+
+medinetAxios.interceptors.response.use(
+    (response) => response,
+    ErrorHandler
+);
+
+export default medinetAxios;
