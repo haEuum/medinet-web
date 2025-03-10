55fcd0c9ff7abca795e7219aad508c732a3eab4c
윤효진 <hyojinee@dgsw.hs.kr>
2025-03-10 19:32:31 +0900

    feat: request Interceptor

--- src/libs/axios/requestInterceptor.ts
+++ src/libs/axios/requestInterceptor.ts
@@ -1,1 +1,17 @@
-import { InternalAxiosRequestConfig } from 'axios';
+import { InternalAxiosRequestConfig } from "axios";
+import Token from "@/libs/token/token";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "@/constants/token/token";
+
+export const requestInterceptor = (config: InternalAxiosRequestConfig) => {
+    const accessToken = Token.getToken(ACCESS_TOKEN);
+    const refreshToken = Token.getToken(REFRESH_TOKEN);
+
+    if (!accessToken || !refreshToken) {
+        console.log("인증 정보 없음");
+    } else {
+        config.headers["Authorization"] = `Bearer ${accessToken}`;
+        config.headers["Refresh-Token"] = `Bearer ${refreshToken}`;
+    };
+
+    return config;
+};
