0a1b5bcca0380cb81462ffdd074ab2c77ce27672
윤효진 <hyojinee@dgsw.hs.kr>
2025-03-11 20:26:59 +0900

    feat: customAxios

--- src/libs/axios/customAxios.ts
+++ src/libs/axios/customAxios.ts
@@ -1,5 +1,8 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "@/constants/token/token";
+import { errorHandler } from "./errorHandler";
+import { requestInterceptor } from "./requestInterceptor";
+import { responseErrorInterceptor } from "./responseErrorInterceptor";
 
 interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
     retry?: boolean;
@@ -22,9 +25,19 @@ const onRefreshed = (token: string) => {
 };
 
 medinetAxios.interceptors.request.use(
-
+    (config: CustomAxiosRequestConfig) => {
+        return requestInterceptor(config);
+    },
+    (error: AxiosError) => {
+        return errorHandler(error);
+    },
 );
 
 medinetAxios.interceptors.response.use(
-
+    (response) => {
+        return response;
+    },
+    (error: AxiosError) => {
+        return responseErrorInterceptor(error);
+    },
 );
