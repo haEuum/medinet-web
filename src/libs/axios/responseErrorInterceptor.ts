1905b581e4928851213647bfcf69c5690df35eab
윤효진 <hyojinee@dgsw.hs.kr>
2025-03-11 21:24:11 +0900

    feat: addSubscriber

--- src/libs/axios/responseErrorInterceptor.ts
+++ src/libs/axios/responseErrorInterceptor.ts
@@ -1,7 +1,20 @@
-import axios, { AxiosError, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
+import axios, { AxiosError } from 'axios';
 import Token from '@/libs/token/token';
 import { ACCESS_TOKEN, REFRESH_TOKEN } from '@/constants/token/token';
+import { errorHandler } from './errorHandler';
+
+let isRefreshing = false;
+let subscribers: ((token: string) => void)[] = [];
+
+const onRefreshed = (token: string) => {
+    subscribers.forEach((callback) => callback(token));
+    subscribers = [];
+};
+
+const addSubscriber = (callback: (token: string) => void) => {
+    subscribers.push(callback);
+};
 
 export const responseErrorInterceptor = (error: AxiosError) => {
-
+    
 };
