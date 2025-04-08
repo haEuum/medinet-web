c4c354b392991d78fb874a0d13e24254bc84373a
박상민 <a01071397193@gmail.com>
2025-04-08 10:25:02 +0900

    Update :: Auth API Data

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,11 +1,11 @@
-import { MedinetAxios } from "@/libs/axios/customAxios";
+import axios from "axios";
 import { Login, SignUp, AuthResponse, NewAccessToken } from "@/types/auth/auth.type";
 
 const SERVER_URL = process.env.VITE_SERVER_URL;
 
 export const login = async ( loginData: Login ):Promise<AuthResponse> => {
     try {
-        const { data } = await MedinetAxios.post<AuthResponse>(`${SERVER_URL}/login`, loginData);
+        const { data } = await axios.post<AuthResponse>(`${SERVER_URL}/login`, loginData);
         return data;
     } catch (error) {
         throw new Error("로그인 요청 실패");
@@ -14,7 +14,7 @@ export const login = async ( loginData: Login ):Promise<AuthResponse> => {
 
 export const refresh = async ( refreshToken: { refreshToken: string | null } ): Promise<NewAccessToken> => {
     try {
-        const { data } = await MedinetAxios.post<NewAccessToken>(`${SERVER_URL}/reissue`, refreshToken);
+        const { data } = await axios.post<NewAccessToken>(`${SERVER_URL}/reissue`, refreshToken);
         return data;
     } catch (error) {
         throw new Error("리프레시 에러");
@@ -23,7 +23,7 @@ export const refresh = async ( refreshToken: { refreshToken: string | null } ): 
 
 export const signup = async ( signUpData: SignUp ): Promise<AuthResponse> => {
     try {
-        const { data } = await MedinetAxios.post<AuthResponse>(`${SERVER_URL}/signup`, signUpData);
+        const { data } = await axios.post<AuthResponse>(`${SERVER_URL}/signup`, signUpData);
         return data;
     } catch (error) {
         throw new Error("회원가입 요청 실패");
