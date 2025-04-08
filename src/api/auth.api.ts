73c68c82649f3f6da308d5edb3b8d9cd2bbccc83
박상민 <a01071397193@gmail.com>
2025-04-08 11:05:19 +0900

    Update :: Login Hook

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Login, SignUp, AuthResponse, NewAccessToken } from "@/types/auth/auth.type";
 
-const SERVER_URL = process.env.VITE_SERVER_URL;
+const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 
 export const login = async ( loginData: Login ):Promise<AuthResponse> => {
     try {
@@ -9,7 +9,7 @@ export const login = async ( loginData: Login ):Promise<AuthResponse> => {
         return data;
     } catch (error) {
         throw new Error("로그인 요청 실패");
-    };
+    }
 };
 
 export const refresh = async ( refreshToken: { refreshToken: string | null } ): Promise<NewAccessToken> => {
@@ -27,5 +27,5 @@ export const signup = async ( signUpData: SignUp ): Promise<AuthResponse> => {
         return data;
     } catch (error) {
         throw new Error("회원가입 요청 실패");
-    };
+    }
 };
