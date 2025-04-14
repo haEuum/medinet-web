88d7dee705bf7821f963dc0f619f4669c440e48b
윤효진 <hyojinee@dgsw.hs.kr>
2025-04-14 17:45:56 +0900

    fix: forder name

--- src/components/signup/step1/index.tsx
+++ src/components/signup/step1/index.tsx
@@ -1,6 +1,29 @@
+import TextField from "@/components/common/textField";
+
 const Step1 = () => {
     return (
-        <div></div>
+        <div className="signup-step1">
+            <div className="signup-step1-input-group">
+                <TextField 
+                    label="이메일"
+                    type="email"
+                    name="email"
+                    placeholder="이메일을 입력해주세요"
+                    value=""
+                    width="300px"
+                    onChange={() => {}}
+                />
+                <TextField 
+                    label="비밀번호"
+                    type="password"
+                    name="password"
+                    placeholder="비밀번호를 입력해주세요"
+                    value=""
+                    width="320px"
+                    onChange={() => {}}
+                />
+            </div>
+        </div>
     );
 };
 
