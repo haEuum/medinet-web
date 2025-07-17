442ce0dc4b8e6710b84dc9e709acb7ba14f4cce3
박상민 <a01071397193@gmail.com>
2025-07-17 09:35:20 +0900

    fix :: 오타 수정

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -1,24 +1,24 @@
-import styles from "./style.module.scss";
 import { ButtonProps } from "@/types/ui/button/button.type";
 import classNames from "classnames";
+import "./style.scss";
 
 const Button = ({ size, color, onClick, text }: ButtonProps) => {
     const sizeClass = {
-        "Extra-Large": styles.extraLarge,
-        "Large": styles.large,
-        "Medium": styles.medium,
-        "Small": styles.small,
+        "Extra-Large": "extra-large",
+        "Large": "large",
+        "Medium": "medium",
+        "Small": "small",
     }[size];
 
     const colorClass = {
-        Primary: styles.primary,
-        AlterNative: styles.alterNative,
-        Assistive: styles.assistive,
-        Gray: styles.gray,
+        Primary: "primary",
+        AlterNative: "alternative",
+        Assistive: "assistive",
+        Gray: "gray",
     }[color];
 
     return (
-        <button className={classNames(styles.button, sizeClass, colorClass)} onClick={onClick}>
+        <button className={classNames("button", sizeClass, colorClass)} onClick={onClick}>
             {text}
         </button>
     );
